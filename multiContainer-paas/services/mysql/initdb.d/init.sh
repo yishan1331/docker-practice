49ae250032d9a0e206a607d3501e714db0065ff9
yishan Tsai <yishanjob13@gmail.com>
2021-05-04 14:41:38 +0800

    two nginx+flask+redis+mysql+postgres+celeryworker finally

--- multiContainer-paas/services/mysql/initdb.d/init.sh
+++ multiContainer-paas/services/mysql/initdb.d/init.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
-mysql -uroot -psapido -e "create database paas_dashboard"
-mysql -uroot -psapido -e "create database IOT"
-mysql -uroot -psapido IOT < ./iot.sql
-mysql -uroot -psapido -e "create database APIDOC"
-mysql -uroot -psapido APIDOC < ./APIDOC.sql
+# mysql -uroot -psapido -e "create database paas_dashboard"
+# mysql -uroot -psapido -e "create database IOT"
+# mysql -uroot -psapido IOT < ./chunzu_iot.sql
+# mysql -uroot -psapido -e "create database APIDOC"
+# mysql -uroot -psapido APIDOC < ./APIDOC.sql
+
 mysql -uroot -psapido -e "create user 'paas'@'%' IDENTIFIED BY 'sapido@admin';"
 mysql -uroot -psapido -e "grant ALL PRIVILEGES ON *.* TO 'paas'@'%';"
 mysql -uroot -psapido -e "FLUSH PRIVILEGES;"

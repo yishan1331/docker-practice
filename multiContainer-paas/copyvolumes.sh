8a2ff77f12fd4c31b8da1700c6c44b7f589c015c
Yishan <yishanjob13@gmail.com>
2021-05-25 11:13:37 +0800

    Debug

--- multiContainer-paas/copyvolumes.sh
+++ multiContainer-paas/copyvolumes.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-sudo cp ./services/nginx/html/web/ /var/lib/docker/volumes/web-html/_data/
-sudo cp ./services/nginx/html/api/ /var/lib/docker/volumes/api-html/_data/
-sudo cp ./services/flask/dashboard /var/lib/docker/volumes/paas-dashboard/_data/
+sudo cp -R ./services/nginx/html/web/* /var/lib/docker/volumes/web-html/_data/
+sudo cp -R ./services/nginx/html/api/* /var/lib/docker/volumes/api-html/_data/
+sudo cp -R ./services/flask/dashboard/* /var/lib/docker/volumes/paas-dashboard/_data/
 
 docker-compose restart

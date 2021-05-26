686b0697725bb69ac06c2f1456d149166b5ee837
Yishan <yishanjob13@gmail.com>
2021-05-26 09:28:16 +0800

    每個service都加上environment 時區 php service crontab&rsyslog start

--- multiContainer-paas/copyvolumes.sh
+++ multiContainer-paas/copyvolumes.sh
@@ -4,4 +4,6 @@ sudo cp -R ./services/nginx/html/api/* /var/lib/docker/volumes/api-html/_data/
 sudo cp -R ./services/flask/dashboard/* /var/lib/docker/volumes/paas-dashboard/_data/
 
 docker-compose restart
+docker exec -it php /etc/init.d/rsyslog start
 docker exec -it php /etc/init.d/cron start
+
--- multiContainer-paas/php_cron_start.sh
+++ multiContainer-paas/php_cron_start.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
+docker exec -it php /etc/init.d/rsyslog start
 docker exec -it php /etc/init.d/cron start

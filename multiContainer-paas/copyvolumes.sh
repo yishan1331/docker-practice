f714bd7ecc507e0e31997f585aa9fec191dae378
Yishan <yishanjob13@gmail.com>
2021-05-27 14:57:37 +0800

    debug

--- multiContainer-paas/copyvolumes.sh
+++ multiContainer-paas/copyvolumes.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 sudo cp -R ./services/nginx/html/web/* /var/lib/docker/volumes/web-html/_data/
+# sudo chown -R www-data:www-data /var/lib/docker/volumes/web-html/_data/*
 sudo cp -R ./services/nginx/html/api/* /var/lib/docker/volumes/api-html/_data/
+# sudo chown -R www-data:www-data /var/lib/docker/volumes/api-html/_data/*
 sudo cp -R ./services/flask/dashboard/* /var/lib/docker/volumes/paas-dashboard/_data/
+# sudo chown -R www-data:www-data /var/lib/docker/volumespaas-dashboard/_data/*
 
 docker-compose restart
 docker exec -it php /etc/init.d/rsyslog start

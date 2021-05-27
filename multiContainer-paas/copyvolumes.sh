6978399653d7ccea9e8bd312e3f145d5f57b8495
Yishan <yishanjob13@gmail.com>
2021-05-27 17:26:01 +0800

    debug

--- multiContainer-paas/copyvolumes.sh
+++ multiContainer-paas/copyvolumes.sh
@@ -6,6 +6,9 @@ sudo cp -R ./services/nginx/html/api/* /var/lib/docker/volumes/api-html/_data/
 sudo cp -R ./services/flask/dashboard/* /var/lib/docker/volumes/paas-dashboard/_data/
 # sudo chown -R www-data:www-data /var/lib/docker/volumespaas-dashboard/_data/*
 
+sudo docker cp $(sudo docker ps | grep flask_app | head -1 | awk '{ print $1 }'):/root/.ssh/id_rsa.pub ~/.ssh/authorized_keys
+sudo chmod 644 ~/.ssh/authorized_keys
+
 docker-compose restart
 docker exec -it php /etc/init.d/rsyslog start
 docker exec -it php /etc/init.d/cron start

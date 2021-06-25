6db43fd8c8e7981f194b13264698e0970387aab8
Yishan <yishanjob13@gmail.com>
2021-06-25 20:53:31 +0800

    *更新multiContainer-PaaS about html

--- multiContainer-paas/services/nginx/html/api/mes_modules/globalvar.php
+++ multiContainer-paas/services/nginx/html/api/mes_modules/globalvar.php
@@ -2,11 +2,6 @@
 global $IP;
 if (empty($IP)) {
     $IP = '192.6.6.230'; // 單機版serve
-    // $IP = 'localhost'; // 機聯網serve
-    // $IP = '211.75.243.185'; // 本地
-    // $IP = '211.75.243.186'; // 單機本地
 }
 $publicIP = $IP;
-// $publicPort = "3687"; // 機聯網serve
-// $publicPort = "23687"; // 單機本地
 $publicPort = "3687" ;// 單機版serve
